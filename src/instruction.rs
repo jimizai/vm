2b223eabac76025327f601f811ee6289c709d962
dingyike.dyk <dingyike.dyk@alibaba-inc.com>
2021-05-24 18:15:35 +0800

    init: initial

---
+++ src/instruction.rs
@@ -0,0 +1,170 @@
+pub type Offset = u16;
+pub type Imm = u16;
+pub type TrapVector = u8;
+pub type Flag = bool;
+pub type Register = usize;
+
+pub struct Opcode(u16);
+
+impl Opcode {
+    pub fn _raw_value(&self) -> u16 {
+        self.0
+    }
+
+    pub fn op_type(&self) -> u8 {
+        ((self.0 >> 12) & 0xF) as u8
+    }
+
+    pub fn xooo(&self) -> usize {
+        ((self.0 >> 9) & 0x7) as usize
+    }
+
+    pub fn oxoo(&self) -> usize {
+        ((self.0 >> 6) & 0x7) as usize
+    }
+
+    pub fn _ooxo(&self) -> usize {
+        ((self.0 >> 3) & 0x7) as usize
+    }
+
+    pub fn ooox(&self) -> usize {
+        (self.0 & 0x7) as usize
+    }
+
+    pub fn offset6(&self) -> u16 {
+        Opcode::sign_extend(self.0 & 0xFFFF, 6)
+    }
+
+    pub fn offset9(&self) -> u16 {
+        Opcode::sign_extend(self.0 & 0xFFFF, 9)
+    }
+
+    pub fn offset11(&self) -> u16 {
+        Opcode::sign_extend(self.0 & 0xFFFF, 11)
+    }
+
+    pub fn imm5(&self) -> u16 {
+        Opcode::sign_extend(self.0 & 0xFFFF, 5)
+    }
+
+    pub fn bit5(&self) -> bool {
+        ((self.0 & 0b0000_0000_0010_0000) >> 5) == 1
+    }
+
+    pub fn bit11(&self) -> bool {
+        ((self.0 & 0b0000_1000_0000_0000) >> 5) == 1
+    }
+
+    pub fn n(&self) -> bool {
+        ((self.0 >> 11) & 0x1) == 1
+    }
+
+    pub fn z(&self) -> bool {
+        ((self.0 >> 10) & 0x1) == 1
+    }
+
+    pub fn p(&self) -> bool {
+        ((self.0 >> 9) & 0x1) == 1
+    }
+
+    pub fn trap_vector(&self) -> u8 {
+        (self.0 & 0xFF) as u8
+    }
+
+    #[inline]
+    pub fn sign_extend(data: u16, size: u16) -> u16 {
+        (((data << (16 - size)) as i16) >> (16 - size)) as u16
+    }
+}
+
+impl From<u16> for Opcode {
+    fn from(opcode: u16) -> Opcode {
+        Opcode(opcode)
+    }
+}
+
+#[derive(Debug)]
+pub enum Instruction {
+    AddReg(Register, Register, Register),
+    AddImm(Register, Register, Imm),
+    AndReg(Register, Register, Register),
+    AndImm(Register, Register, Imm),
+    Br(Flag, Flag, Flag, Offset),
+    Jmp(Register),
+    Jsr(Offset),
+    Jsrr(Register),
+    Ld(Register, Offset),
+    Ldi(Register, Offset),
+    Ldr(Register, Register, Offset),
+    Lea(Register, Offset),
+    Not(Register, Register),
+    Rti(),
+    St(Register, Offset),
+    Sti(Register, Offset),
+    Str(Register, Register, Offset),
+    Trap(TrapVector),
+    Reserved(),
+}
+
+impl Instruction {
+    pub fn new<T: Into<Opcode>>(opcode: T) -> Option<Instruction> {
+        let opcode: Opcode = opcode.into();
+        match opcode.op_type() {
+            0b0001 => match opcode.bit5() {
+                false => Some(Instruction::AddReg(
+                    opcode.xooo(),
+                    opcode.oxoo(),
+                    opcode.ooox(),
+                )),
+                true => Some(Instruction::AddImm(
+                    opcode.xooo(),
+                    opcode.oxoo(),
+                    opcode.imm5(),
+                )),
+            },
+            0b0101 => match opcode.bit5() {
+                false => Some(Instruction::AndReg(
+                    opcode.xooo(),
+                    opcode.oxoo(),
+                    opcode.ooox(),
+                )),
+                true => Some(Instruction::AndImm(
+                    opcode.xooo(),
+                    opcode.oxoo(),
+                    opcode.imm5(),
+                )),
+            },
+            0b0000 => Some(Instruction::Br(
+                opcode.n(),
+                opcode.z(),
+                opcode.p(),
+                opcode.offset9(),
+            )),
+            0b1100 => Some(Instruction::Jmp(opcode.oxoo())),
+            0b0100 => match opcode.bit11() {
+                false => Some(Instruction::Jsrr(opcode.oxoo())),
+                true => Some(Instruction::Jsr(opcode.offset11())),
+            },
+            0b0010 => Some(Instruction::Ld(opcode.xooo(), opcode.offset9())),
+            0b1010 => Some(Instruction::Ldi(opcode.xooo(), opcode.offset9())),
+            0b0110 => Some(Instruction::Ldr(
+                opcode.xooo(),
+                opcode.oxoo(),
+                opcode.offset6(),
+            )),
+            0b1110 => Some(Instruction::Lea(opcode.xooo(), opcode.offset9())),
+            0b1001 => Some(Instruction::Not(opcode.xooo(), opcode.oxoo())),
+            0b1000 => Some(Instruction::Rti()),
+            0b0011 => Some(Instruction::St(opcode.xooo(), opcode.offset9())),
+            0b1011 => Some(Instruction::Sti(opcode.xooo(), opcode.offset9())),
+            0b0111 => Some(Instruction::Str(
+                opcode.xooo(),
+                opcode.oxoo(),
+                opcode.offset6(),
+            )),
+            0b1111 => Some(Instruction::Trap(opcode.trap_vector())),
+            0b1101 => Some(Instruction::Reserved()),
+            _ => None,
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,291 @@
+use instruction::{Instruction, Opcode};
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+use termios::*;
+
+mod instruction;
+
+enum Registers {
+    R0 = 0,
+    _R1,
+    _R2,
+    _R3,
+    _R4,
+    _R5,
+    _R6,
+    R7,
+    PC,
+    COUNT,
+}
+
+pub struct Flags {
+    n: bool,
+    z: bool,
+    p: bool,
+}
+
+impl Flags {
+    pub fn update(&mut self, reg: u16) {
+        match reg {
+            0x0000 => {
+                self.n = false;
+                self.z = true;
+                self.p = false
+            }
+            0x0001..=0x7FFF => {
+                self.n = false;
+                self.p = true;
+                self.z = false;
+            }
+            0x8000..=0xFFFF => {
+                self.n = true;
+                self.p = false;
+                self.z = false
+            }
+        };
+    }
+}
+
+impl From<u8> for Flags {
+    fn from(flags: u8) -> Flags {
+        Flags {
+            n: ((flags & 0x4) >> 2) == 1,
+            z: ((flags & 0x2) >> 1) == 1,
+            p: (flags & 0x1) == 1,
+        }
+    }
+}
+
+enum TrapCode {
+    Getc(),
+    Out(),
+    Puts(),
+    In(),
+    Putsp(),
+    Halt(),
+    Unknown(),
+}
+
+impl From<u8> for TrapCode {
+    fn from(vector: u8) -> TrapCode {
+        match vector {
+            0x20 => TrapCode::Getc(),
+            0x21 => TrapCode::Out(),
+            0x22 => TrapCode::Puts(),
+            0x23 => TrapCode::In(),
+            0x24 => TrapCode::Putsp(),
+            0x25 => TrapCode::Halt(),
+            _ => TrapCode::Unknown(),
+        }
+    }
+}
+
+enum MemoryMappedRegisters {
+    KBSR = 0xFE00,
+    KBDR = 0xFE02,
+}
+
+fn mem_read(mem: &mut [u16], addr: u16) -> u16 {
+    if addr == MemoryMappedRegisters::KBSR as u16 {
+        let mut buffer = [0; 1];
+        std::io::stdin().read_exact(&mut buffer).unwrap();
+        if buffer[0] != 0 {
+            mem[MemoryMappedRegisters::KBDR as usize] = 1 << 15;
+            mem[MemoryMappedRegisters::KBSR as usize] = buffer[0] as u16
+        } else {
+            mem[MemoryMappedRegisters::KBSR as usize] = 0
+        }
+    }
+    mem[addr as usize]
+}
+
+fn mem_write(mem: &mut [u16], addr: u16, val: u16) {
+    mem[addr as usize] = val
+}
+
+fn abort() {
+    std::process::exit(1);
+}
+
+fn get16(mem: &[u8], addr: usize) -> u16 {
+    ((mem[addr] as u16) << 8) + mem[addr + 1] as u16
+}
+
+fn read_image(mem: &mut [u16], image_path: &str) -> u32 {
+    let path = Path::new(&image_path);
+    let mut file = File::open(&path).expect("Coudn't open file");
+    const SIZE: u32 = std::u16::MAX as u32 * 2 - 2;
+    let mut mem_buffer: [u8; SIZE as usize] = [0; SIZE as usize];
+    file.read(&mut mem_buffer).expect("Coudn't read file.");
+    let length = file.metadata().unwrap().len();
+    let base = get16(&mem_buffer, 0) as usize;
+    for i in (2..length).step_by(2) {
+        mem[base + (i / 2 - 1) as usize] = get16(&mem_buffer, i as usize);
+    }
+    length as u32
+}
+
+fn run() {
+    println!("run");
+    let mut memory: [u16; std::u16::MAX as usize] = [0; std::u16::MAX as usize];
+
+    read_image(&mut memory, "sample_apps/2048.obj");
+
+    let mut regs: [u16; Registers::COUNT as usize] = [0; Registers::COUNT as usize];
+    let mut flags = Flags::from(0);
+
+    let pc_start: u16 = 0x3000;
+    regs[Registers::PC as usize] = pc_start;
+
+    let mut running: bool = true;
+
+    while running {
+        let opcode = Opcode::from(mem_read(&mut memory, regs[Registers::PC as usize]));
+        let instruction = Instruction::new(opcode).unwrap();
+        regs[Registers::PC as usize] += 1;
+
+        println!("{}", regs[Registers::PC as usize]);
+
+        match instruction {
+            Instruction::Br(n, z, p, offset) => {
+                if (n & flags.n) || (z & flags.z) || (p & flags.p) {
+                    regs[Registers::PC as usize] += offset
+                }
+            }
+            Instruction::AddReg(dr, sr1, sr2) => {
+                regs[dr] = regs[sr1] + regs[sr2];
+                flags.update(regs[dr])
+            }
+            Instruction::AddImm(dr, sr1, imm5) => {
+                regs[dr] = regs[sr1] + (imm5 as u16);
+                flags.update(regs[dr])
+            }
+            Instruction::AndReg(dr, sr1, sr2) => {
+                regs[dr] = regs[sr1] & regs[sr2];
+                flags.update(regs[dr])
+            }
+            Instruction::AndImm(dr, sr1, imm5) => {
+                regs[dr] = regs[sr1] & (imm5 as u16);
+                flags.update(regs[dr])
+            }
+            Instruction::Jmp(base_r) => {
+                regs[Registers::R7 as usize] = regs[Registers::PC as usize];
+                regs[Registers::PC as usize] = regs[base_r]
+            }
+            Instruction::Jsr(offset) => {
+                regs[Registers::R7 as usize] = regs[Registers::PC as usize];
+                regs[Registers::PC as usize] += offset
+            }
+            Instruction::Jsrr(base_r) => {
+                regs[Registers::R7 as usize] = regs[Registers::PC as usize];
+                regs[Registers::PC as usize] = regs[base_r]
+            }
+            Instruction::Ld(dr, offset) => {
+                regs[dr] = mem_read(&mut memory, regs[Registers::PC as usize] + offset);
+                flags.update(regs[dr])
+            }
+            Instruction::Ldi(dr, offset) => {
+                let addr = mem_read(&mut memory, regs[Registers::PC as usize] + offset);
+                regs[dr] = mem_read(&mut memory, addr);
+                flags.update(regs[dr])
+            }
+            Instruction::Ldr(dr, base_r, offset) => {
+                regs[dr] = mem_read(&mut memory, regs[base_r] + offset);
+                flags.update(regs[dr])
+            }
+            Instruction::Lea(dr, offset) => {
+                regs[dr] = regs[Registers::PC as usize] + offset;
+                flags.update(regs[dr])
+            }
+            Instruction::Not(dr, sr) => {
+                regs[dr] = !regs[sr];
+                flags.update(regs[dr])
+            }
+            Instruction::Rti() => abort(),
+            Instruction::St(sr, offset) => {
+                mem_write(&mut memory, regs[Registers::PC as usize] + offset, regs[sr]);
+            }
+            Instruction::Sti(_sr, _offset) => {
+                abort()
+                // mem_write(&mut memory, regs[Registers::PC as usize] + offset, regs[sr])
+            }
+            Instruction::Str(sr, base_r, offset) => {
+                mem_write(&mut memory, regs[base_r] + offset, regs[sr])
+            }
+            Instruction::Trap(trap_vector) => match TrapCode::from(trap_vector) {
+                TrapCode::Getc() => {
+                    let mut buffer = [0; 1];
+                    std::io::stdin().read_exact(&mut buffer).unwrap();
+                    regs[Registers::R0 as usize] = buffer[0] as u16
+                }
+                TrapCode::Out() => {
+                    let c = regs[Registers::R0 as usize] as u8;
+                    print!("{}", c as char);
+                }
+                TrapCode::Puts() => {
+                    for c in &memory[regs[Registers::R0 as usize] as usize..] {
+                        let c8 = (c & 0xFF) as u8;
+                        if c8 != 0x00 {
+                            print!("{}", c8 as char);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+                TrapCode::In() => {
+                    print!("Enter a character: ");
+                    regs[Registers::R0 as usize] = std::io::stdin()
+                        .bytes()
+                        .next()
+                        .and_then(|result| result.ok())
+                        .map(|byte| byte as u16)
+                        .unwrap();
+                    // println!("[*] IN");
+                }
+                // OUTPUT A BYTE STRING
+                TrapCode::Putsp() => {
+                    for c in &memory[regs[Registers::R0 as usize] as usize..] {
+                        let b1 = (*c >> 8) as u8;
+                        let b2 = (*c & 0xff) as u8;
+                        if b1 != 0 {
+                            print!("{}", b1 as char);
+                            if b2 != 0 {
+                                print!("{}", b2 as char);
+                            }
+                        }
+                    }
+                    // println!("[*] PUTSP");
+                }
+                TrapCode::Halt() => {
+                    println!("[!] HALT");
+                    running = false;
+                }
+                TrapCode::Unknown() => {
+                    println!("[!] UNKNOWN TRAPCODE");
+                    abort();
+                }
+            },
+            Instruction::Reserved() => {
+                // println!("[*] RES");
+            }
+        }
+    }
+}
+
+fn main() {
+    // https://stackoverflow.com/questions/26321592/how-can-i-read-one-character-from-stdin-without-having-to-hit-enter
+    let stdin = 0; // couldn't get std::os::unix::io::FromRawFd to work
+                   // on /dev/stdin or /dev/tty
+    let termios = Termios::from_fd(stdin).unwrap();
+    let mut new_termios = termios.clone(); // make a mutable copy of termios
+                                           // that we will modify
+    new_termios.c_iflag &= IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL | IXON;
+    new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
+    tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
+
+    run();
+
+    tcsetattr(stdin, TCSANOW, &termios).unwrap(); // reset the stdin to
+                                                  // original termios data
+}
